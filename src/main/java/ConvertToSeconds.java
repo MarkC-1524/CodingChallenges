efb6ca247282d6b3551372432a975a7f1b3abe68
markc-1524 <markc1524@gmail.com>
2023-03-14 07:44:56 -0700

    Week of March 13 Challenge

--- src/main/java/ConvertToSeconds.java
+++ src/main/java/ConvertToSeconds.java
@@ -7,15 +7,19 @@ public class ConvertToSeconds {
         int minutes = 0;
 
         System.out.println("To exit converter enter a negative number");
-        while(minutes >= 0){
+        while(minutes >= 0) {
             System.out.print("Enter a number: ");
-            minutes = scan.nextInt();
+            try {
+                minutes = scan.nextInt();
+                System.out.println(convert(minutes));
+            } catch (Exception e){
+                System.out.println("Try a number");
+            }
             scan.nextLine();
-            System.out.println(convert(minutes));
         }
         System.out.println("This is not a time machine. " + minutes + " is an invalid amount of time");
     }
- 
+
     public static int convert(int minutes){
         return minutes*60;
     }
